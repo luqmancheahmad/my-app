5e13b72aee1d63f7a0214d1e789bcac6e0320f94
luqmanCA <luqman.cheahmad@gmail.com>
2023-07-05 23:00:39 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import { useMemo } from 'react';
+import {GoogleMaps, useLoadScript, Marker } from 'google-map-react';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React you 
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+function App() {
+  return <div>Map</div>;
+}
+export default App;
